0e4bd3b9df5e9b1127ae4b51fd8d3218245c08e1
mandarin <nikita.rudakov.015@gmail.com>
2022-02-02 22:55:49 +0200

    First code

--- FProg/Program.cs
+++ FProg/Program.cs
@@ -1,5 +1,16 @@
-﻿
-namespace first_app
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FProg
 {
-    static void 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
 }
