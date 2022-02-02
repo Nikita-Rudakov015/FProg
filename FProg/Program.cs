5be63eb9d46047b3fbe79ba7e514435bcb9fa93d
mandarin <nikita.rudakov.015@gmail.com>
2022-02-02 23:06:34 +0200

    Second code

--- FProg/Program.cs
+++ FProg/Program.cs
@@ -10,7 +10,7 @@ namespace FProg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("My friends");
         }
     }
 }
