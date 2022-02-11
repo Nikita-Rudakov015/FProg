1e2fbcf8331aa7208c2ea42492013121c5e29ed6
mandarin <nikita.rudakov.015@gmail.com>
2022-02-11 23:04:33 +0200

    lesson-3-loops

---
+++ lesson-3-condition-loops/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace lesson_3_condition_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string message1 = Console.ReadLine();
+            if (float.TryParse(message1, out float digit1))
+            {
+                //Console.WriteLine(digit + 1);
+            }
+            else { Console.WriteLine("Input is incorrect"); }
+            string message2 = Console.ReadLine();
+            if (float.TryParse(message2, out float digit2))
+            {
+                //Console.WriteLine(digit + 1);
+            }
+            else { Console.WriteLine("Input is incorrect"); }
+            string message3 = Console.ReadLine();
+            if (int.TryParse(message3, out int digit3))
+            {
+                switch (digit3)
+                {
+                    case 1:
+                        Console.WriteLine(digit1 + digit2);
+                        break;
+                    case 2:
+                        Console.WriteLine(digit1 - digit2);
+                        break;
+                    case 3:
+                        Console.WriteLine(digit1 * digit2);
+                        break;
+                    case 4:
+                        Console.WriteLine(digit1 / digit2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else { }
+
+        }
+    }
+}
