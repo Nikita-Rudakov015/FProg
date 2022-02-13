785632174db9d93dd9cec8b3e2d4ac34f96deccb
mandarin <nikita.rudakov.015@gmail.com>
2022-02-13 09:56:49 +0200

    Revert "TimeSpan"
    This reverts commit a3a7100343e7184613ad1b9c2a7a852a7698ff2a.

--- lesson_data_type/Program.cs
+++ lesson_data_type/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Timers;
 
 namespace lesson_data_type
 {
@@ -90,16 +89,6 @@ namespace lesson_data_type
             Console.WriteLine($"The second example: {Math.Abs(x) * Math.Sin(x)}");
             Console.WriteLine($"The 3-id example: {2 * Math.PI * x}");
             Console.WriteLine($"The 4-th example: {Math.Max(x, y)}");
-
-
-            var hours = TimeSpan.FromHours(6);
-            Console.WriteLine(TimeSpan.FromHours(6) + TimeSpan.FromHours(6));
-
-            var timer = new Timer();
-            timer.Interval = TimeSpan.FromSeconds(2).TotalMilliseconds;
-            timer.Elapsed += (a, e) => Console.WriteLine("Some");
-            timer.Start();
-            Console.ReadKey();
         }
     }
 }
